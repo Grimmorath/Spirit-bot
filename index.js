135b88f81b743155d5e0b4548e7b320160a73670
Grimmorath <58078864+Grimmorath@users.noreply.github.com>
2019-11-22 16:44:35 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var message = 'Ping';
+
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
- }
- });
+	
+	var uppercase = msg.content.toUpperCase()
+	
+	 if (uppercase === 'PING' ) {
+	 msg.reply('pong');
+	 }
+	 });
 
-client.login('NjQ3NDQyNTM3Nzk5MjIxMjU5.XdfwHg.bWCI37ZBqqJenwtr9rmWKf_ZiCM');
+client.login('NjQ3NDQyNTM3Nzk5MjIxMjU5.Xdf95w.i8gu6sfmn3PmDWBTAnoxBY9r6s8');
